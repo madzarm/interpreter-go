ba57d061591ac2c61128c0434c11645bbd84e3ce
Maksim <madzarmaksim@gmail.com>
2024-02-16 21:51:45 +0100

    add eval to repl

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"GoInterpreter/evaluator"
 	"GoInterpreter/lexer"
 	"GoInterpreter/parser"
 	"bufio"
@@ -38,8 +39,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 func printParserErrors(out io.Writer, errors []string) {

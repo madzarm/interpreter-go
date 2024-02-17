c7bf31d5bed65524166dcb9c7ea66d7ede1344be
Maksim <madzarmaksim@gmail.com>
2024-02-17 19:32:51 +0100

    add value binding and identifier eval

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"GoInterpreter/evaluator"
 	"GoInterpreter/lexer"
+	"GoInterpreter/object"
 	"GoInterpreter/parser"
 	"bufio"
 	"fmt"
@@ -25,6 +26,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -39,7 +42,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
